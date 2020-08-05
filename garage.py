0a3b25ee09001ced66e2859ca85aad1d57aa5583
Scott Wambold <scottwambold@Nexcess-AST-000365.local>
2020-08-05 14:29:45 -0400

    version .1

---
+++ garage.py
@@ -0,0 +1,32 @@
+#!/bin/python
+from race import save_car
+
+# Garage - Car manager
+
+# Load car
+
+def show_car(car):
+
+def new_car():
+
+	in_name = input("Car name: ")
+
+	print("Chassis Types")
+	print("s - streamlined")
+	print("n - neutral")
+	print("c - cubic")
+	in_cha = input("Car chassis: ")
+
+	print("Chassis Types")
+	print("i - fission")
+	print("u - fusion")
+	in_eng = input("Car name: ")
+
+	
+
+if __name__ == "__main__":
+	print("Hotrod Garage")
+	print("n - make a new car")
+	print("l - load an exsisting car")
+	option_1 = input("What would you like to do? ")
+
